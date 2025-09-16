23dd4089bc77c7fd5220bd11f95dd87b9b5c0f67
JuanRacchumi <juan.racchumi.dev@gmail.com>
2025-09-16 16:51:43 -0500

    Update routes of certificado

--- src/app/routes/certificado.routes.ts
+++ src/app/routes/certificado.routes.ts
@@ -5,7 +5,7 @@ import { authToken } from '../middleware/authMiddleware'
 const router = Router()
 
 router.get('/paginate', authToken, CertificadoController.getCertificadosPaginated)
-router.get('/codigo/:codigo', authToken, CertificadoController.getCertificadoPorCodigo)
+router.get('/codigo/:codigo', CertificadoController.getCertificadoPorCodigo)
 router.get('/download/:id', authToken, CertificadoController.downloadPorId)
 router.get('/download/name/:filename', authToken, CertificadoController.downloadPorFilename)
 router.get('/', authToken, CertificadoController.getCertificados)
